76ed5446fd30dbc4a57964bdf79e02abe67f048d
GOKULJAYANTH RAMACHANDRAN <gokuljayanth@HAT-ADMINs-MacBook-Pro.local>
2023-07-26 10:20:13 +0530

    Implemented UI

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # get 'main/home'
+  root 'main#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
